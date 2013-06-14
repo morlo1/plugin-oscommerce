4a0e339c275bd10e4b3c55d63dfa799be74801e6
Sergej <s.veliseicik@evp.lt>
2013-06-14 14:49:51 +0300

    Migrating to Paysera.com, minor improvements

---
+++ README.txt
@@ -0,0 +1,7 @@
+# -- INSTRUCTION --
+
+1. BACKUP ALL FILES!!
+2. Copy files to osCommerce directory;
+3. In admin panel active payment method, select it, press edit and fill in all information;
+
+If any problems occur write to support@paysera.com.
---
+++ includes/languages/english/modules/payment/paysera.php
@@ -0,0 +1,49 @@
+<?php
+/*
+  Copyright (c) Rolandas Razma
+  Released under the GNU General Public License
+*/
+
+	define('MODULE_PAYMENT_PAYSERA_TEXT_TITLE', 			'Paysera');
+	define('MODULE_PAYMENT_PAYSERA_TEXT_DESCRIPTION', 		'Paysera.com payment system');
+	
+	define('MODULE_PAYMENT_PAYSERA_STATUS_TITLE', 			'Enable Paysera Module');
+	define('MODULE_PAYMENT_PAYSERA_STATUS_DESCRIPTION', 	'Do you want to accept Paysera payments?');
+	
+	define('MODULE_PAYMENT_PAYSERA_PROJECT_TITLE', 		'ProjectID');
+	define('MODULE_PAYMENT_PAYSERA_PROJECT_DESCRIPTION', 	'Your project ID');
+	
+	define('MODULE_PAYMENT_PAYSERA_SIGNATURE_TITLE', 		'Signature password');
+	define('MODULE_PAYMENT_PAYSERA_SIGNATURE_DESCRIPTION', 'Signature password you can find in your project settings');
+	
+	define('MODULE_PAYMENT_PAYSERA_CURRENCY_TITLE',		'Transaction Currency');
+	define('MODULE_PAYMENT_PAYSERA_CURRENCY_DESCRIPTION',	'The currency to use for transactions');
+	
+	define('MODULE_PAYMENT_PAYSERA_ZONE_TITLE', 			'Payment Zone');
+	define('MODULE_PAYMENT_PAYSERA_ZONE_DESCRIPTION', 		'If a zone is selected, only enable this payment method for that zone.');
+	
+	define('MODULE_PAYMENT_PAYSERA_SORT_ORDER_TITLE', 		'Sort order of display.');
+	define('MODULE_PAYMENT_PAYSERA_SORT_ORDER_DESCRIPTION','Sort order of display. Lowest is displayed first.');
+	
+	define('MODULE_PAYMENT_PAYSERA_TESTING_TITLE', 		'Testing Service');
+	define('MODULE_PAYMENT_PAYSERA_TESTING_DESCRIPTION', 	'Use Testing Service?');
+
+
+	define('MODULE_PAYMENT_PAYSERA_ORDERS_STATUS_20', 		'Payment Confirmed');
+
+
+	define('MODULE_PAYMENT_PAYSERA_CH_ORDER_STATUS_TITLE', 'Change Order Status');
+	define('MODULE_PAYMENT_PAYSERA_CH_ORDER_STATUS_DESCRIPTION', 'Change order status to "'.MODULE_PAYMENT_PAYSERA_ORDERS_STATUS_20.'" after payment done?');
+
+	define('MODULE_PAYMENT_PAYSERA_ORDER_STATUS_ID',       'After payment change order status to');
+
+	
+
+
+
+
+
+
+
+
+
